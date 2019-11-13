a398f8f1ce2fb171829fd699f468fc84c0bd0bd5
Jared Diehl <jareddiehl96@gmail.com>
2019-11-13 15:47:27 -0600

    Updated

--- Project2Sample/src/edu/umsl/java/controller/problem/AddProblemServlet.java
+++ Project2Sample/src/edu/umsl/java/controller/problem/AddProblemServlet.java
@@ -1,11 +1,20 @@
 package edu.umsl.java.controller.problem;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import edu.umsl.java.dao.CategoryDao;
+import edu.umsl.java.dao.CategoryDaoImpl;
+import edu.umsl.java.dao.ProblemDao;
+import edu.umsl.java.dao.ProblemDaoImpl;
+import edu.umsl.java.model.Problem;
 
 /**
  * Servlet implementation class AddProblemServlet
@@ -32,7 +41,61 @@ public class AddProblemServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		try {
+			String title = request.getParameter("title");
+			String categoryId = request.getParameter("categoryId");
+			String content = request.getParameter("content");
+
+			ProblemDao problemDao = new ProblemDaoImpl();
+			CategoryDao categoryDao = new CategoryDaoImpl();
+
+			Map<String, String> errors = new HashMap<String, String>();
+
+			int id = 0;
+
+			if (title.isEmpty()) {
+				errors.put("title", "Cannot be empty!");
+			} else if (title.length() > 50) {
+				errors.put("title", "Max length is 50!");
+			} else if (problemDao.getTitleExists(title)) {
+				errors.put("title", "Already exists!");
+			}
+			if (categoryId != null) {
+				try {
+					id = Integer.parseInt(categoryId);
+					if (id > 0) {
+						if (!categoryDao.getCategoryIdExists(id)) {
+							errors.put("categoryId", "Does not exist!");
+						}
+					} else {
+						errors.put("categoryId", "Invalid id!");
+					}
+				} catch (Exception e) {
+					errors.put("categoryId", "Expected a number!");
+				}
+			}
+			if (content.isEmpty()) {
+				errors.put("content", "Cannot be empty!");
+			}
+
+			if (errors.isEmpty()) {
+				Problem problem = new Problem();
+				problem.setTitle(title);
+				problem.setCategoryId(id);
+				problem.setContent(content);
+				problemDao.addProblem(problem);
+
+				response.sendRedirect("problem");
+			} else {
+				request.setAttribute("title", title);
+				request.setAttribute("categoryId", categoryId);
+				request.setAttribute("content", content);
+				request.setAttribute("errors", errors);
+				getServletContext().getRequestDispatcher("/problem.jsp").forward(request, response);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
