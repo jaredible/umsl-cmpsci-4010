0ee916f8dc772465f8411177fbe14e25178d7081
Jared Diehl <jareddiehl96@gmail.com>
2019-11-21 21:54:23 -0600

    Updated

--- Mindbank/src/net/jaredible/mindbank/filter/NoAuthFilter.java
+++ Mindbank/src/net/jaredible/mindbank/filter/NoAuthFilter.java
@@ -1,6 +1,7 @@
 package net.jaredible.mindbank.filter;
 
 import java.io.IOException;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -9,42 +10,19 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 
-/**
- * Servlet Filter implementation class NoAuthFilter
- */
-@WebFilter("/NoAuthFilter")
+@WebFilter(urlPatterns = { "/login", "/register" })
 public class NoAuthFilter implements Filter {
 
-    /**
-     * Default constructor. 
-     */
-    public NoAuthFilter() {
-        // TODO Auto-generated constructor stub
-    }
+	public NoAuthFilter() {
+	}
 
-	/**
-	 * @see Filter#destroy()
-	 */
 	public void destroy() {
-		// TODO Auto-generated method stub
 	}
 
-	/**
-	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
-	 */
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		// TODO Auto-generated method stub
-		// place your code here
-
-		// pass the request along the filter chain
-		chain.doFilter(request, response);
 	}
 
-	/**
-	 * @see Filter#init(FilterConfig)
-	 */
 	public void init(FilterConfig fConfig) throws ServletException {
-		// TODO Auto-generated method stub
 	}
 
 }
