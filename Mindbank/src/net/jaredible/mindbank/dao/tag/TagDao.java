cc9a142499bb183207af80aaa51e03675fc7ee00
Jared Diehl <jareddiehl96@gmail.com>
2019-11-21 22:45:59 -0600

    Updated

---
+++ Mindbank/src/net/jaredible/mindbank/dao/tag/TagDao.java
@@ -0,0 +1,5 @@
+package net.jaredible.mindbank.dao.tag;
+
+public interface TagDao {
+
+}
---
+++ Mindbank/src/net/jaredible/mindbank/dao/tag/TagDaoImpl.java
@@ -0,0 +1,5 @@
+package net.jaredible.mindbank.dao.tag;
+
+public class TagDaoImpl implements TagDao {
+
+}
