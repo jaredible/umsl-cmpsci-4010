9148e12672fca7d7acb3e3ba01868f950fe5035b
Jared Diehl <jareddiehl96@gmail.com>
2019-11-21 21:55:04 -0600

    Updated

--- Mindbank/src/net/jaredible/mindbank/filter/AuthFilter.java
+++ Mindbank/src/net/jaredible/mindbank/filter/AuthFilter.java
@@ -10,7 +10,7 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 
-@WebFilter(urlPatterns = { "/problem", "/profile" })
+@WebFilter(urlPatterns = { "/logout", "/problem", "/profile" })
 public class AuthFilter implements Filter {
 
 	public AuthFilter() {
